600c104436fe451539afeea05417232e272650ac
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-07-28 11:54:49 -0400

    add basic project folders structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App__header'>
+          <Header />
+        </header>
+        <main className='App__main'>
+          {this.state.hasError && <p className='red'>There was an error! Oh no!</p>}
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={/* */}
+            />
+            <PublicOnlyRoute
+              path={'/login'}
+              component={LoginPage}
+            />
+            <PublicOnlyRoute
+              path={'/register'}
+              component={RegistrationPage}
+            />
+            <PrivateRoute
+              path={'/article/:articleId'}
+              component={/* */}
+            />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+        </main>
+      </div >
+    )
+  }
 }
-
 export default App;
