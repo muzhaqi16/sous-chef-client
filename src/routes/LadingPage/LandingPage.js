17f5bea2f37d58aa8198f97c973940923f7915eb
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-08-01 17:56:18 -0400

    make landing page responsive

--- src/routes/LadingPage/LandingPage.js
+++ src/routes/LadingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import phone from '../../img/iphone.png';
 import './LandingPage.css';
 
 export default class LandingPage extends Component {
@@ -7,8 +8,12 @@ export default class LandingPage extends Component {
         return (
             <>
                 <header className="hero">
-                    <h1>Welcome to <br /> Sous Chef</h1>
-                    <p>The only kitchen assistant that helps you <br /> <span className="stand-out">save food waste</span> and <span className="stand-out">stay healthy</span></p>
+                    <div>
+                        <h1>Welcome to <br /> Sous Chef</h1>
+                        <p>The only kitchen assistant that helps you <br /> <span className="stand-out">save food </span> and <span className="stand-out">stay healthy</span></p>
+                        <button>Try it now!!!</button>
+                    </div>
+                    <img id="mockup" src={phone} alt="app iphone mockup" />
                 </header>
 
                 <section>
