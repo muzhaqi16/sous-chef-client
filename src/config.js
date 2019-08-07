d021da1b08c9a633e7dec702ec782f31a24547e9
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-08-06 23:41:59 -0400

    cahnge api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://sleepy-reaches-47839.herokuapp.com/',
+    API_ENDPOINT: 'http://sleepy-reaches-47839.herokuapp.com/api',
     API_KEY: '910237e9-95fd-4ecf-b17b-4af6605a1f01',
     RECIPE_API_KEY: process.env.RECIPE_API_KEY,
     TOKEN_KEY: 'sous-chef-client-auth-token',
